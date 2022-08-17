4fb5b9a66e3d5c8c940774204ddf6befdb0a3360
Shryler <105290336+Shryler@users.noreply.github.com>
2022-08-17 17:48:49 +0200

    new

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,20 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
 const Countries = () => {
+    const [data, setData] = useState([]);
 
     // Le useEffect se joue lorsque le composant est monté ( [] = Callback )
     useEffect(() => {
         axios
             .get("https://restcountries.com/v3.1/all")
-            .then((res) => console.log(res.data));
+            .then((res) => setData(res.data));
     }, []);
 
     return (
-        <div>
-            <div className="countries">
-                <h1>Countries</h1>
-            </div>
+        <div className="countries">
+            <h1>Countries</h1>
+            <ul>
+                {data.map((country, index) => (
+                    <li key={index}>{country.translations.fra.common}</li>
+                ))}
+            </ul>
         </div>
     );
 };
